ad2b30be2ccb5a6df845dda7bd65604ff3ac2167
Tom Dahlman <catalystvii@gmail.com>
2012-11-10 00:58:25 -0600

    No errors, but no name in user db

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
 	def create
 	 	auth = request.env['omniauth.auth']
+	 	puts auth.to_yaml
+	 	puts auth["user_info"]
 	 	user = User.find_by_provider_and_uid(auth["provider"],auth["uid"]) || User.create_with_omniauth(auth)
 	 	session[:user_id] = user.id
-	 	redirect_to root_url, :notice => "Signed In!"
+	 	redirect_to '/streams', :notice => "Signed In!"
 	end
 end
